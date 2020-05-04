6ad5867fd7f8f80a2b2022a471046b1b30bc1148
morin128 <sdfgh4780@gmail.com>
2020-05-04 18:16:44 +0900

    feat : add new processes

---
+++ src/Application.kt
@@ -0,0 +1,28 @@
+package com.example
+
+import com.example.routes.registerCustomerRoutes
+import com.example.routes.registerOrderRoutes
+import io.ktor.application.Application
+import io.ktor.application.install
+import io.ktor.features.ContentNegotiation
+import io.ktor.http.ContentType
+import io.ktor.serialization.DefaultJsonConfiguration
+import io.ktor.serialization.json
+import kotlinx.serialization.json.Json
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+fun Application.module(testing: Boolean = false) {
+
+    // see. https://ktor.io/servers/features/content-negotiation/serialization-converter.html
+    install(ContentNegotiation) {
+        // "serialization()" is deprecated. Use json instead.
+        json(
+                json = Json(configuration = DefaultJsonConfiguration.copy(prettyPrint = true)),
+                contentType = ContentType.Application.Json
+        )
+    }
+
+    registerCustomerRoutes()
+    registerOrderRoutes()
+}
